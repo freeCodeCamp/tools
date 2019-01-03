cbf38a5113388f65e201563f5d0c554896808687
Randell Dawson <rdawson@onepathtech.com>
2019-01-02 20:43:03 -0800

    fix: first pass at restructure linting cleanup

---
+++ one-off-scripts/get-unknown-repo-prs.js
@@ -0,0 +1,40 @@
+const { owner, repo, octokitConfig, octokitAuth } = require('../constants');
+const octokit = require('@octokit/rest')(octokitConfig);
+
+const { getPRs, getUserInput } = require('../get-prs');
+const { ProcessingLog, rateLimiter } = require('../utils');
+
+octokit.authenticate(octokitAuth);
+
+const log = new ProcessingLog('unknown-repo-prs-with-merge-conflicts');
+log.start();
+(async() => {
+  const { totalPRs, firstPR, lastPR } = await getUserInput('all');
+  const prPropsToGet = ['number', 'user', 'head'];
+  const { openPRs } = await getPRs(totalPRs, firstPR, lastPR, prPropsToGet);
+  if (openPRs.length) {
+    for (let count = 0; count < openPRs.length; count++) {
+      let { number, head: { repo: headRepo } } = openPRs[count];
+      if (headRepo === null) {
+        const {
+          data: { mergeable_state: mergeableState }
+        } = await octokit.pullRequests.get({ owner, repo, number });
+        if (mergeableState === 'dirty' || mergeableState === 'unknown') {
+          log.add(number, { number, mergeableState });
+          console.log(`[${number} (mergeable_state: ${mergeableState})](https://github.com/freeCodeCamp/freeCodeCamp/pull/${number})`);
+        }
+        await rateLimiter(1000);
+      }
+    }
+  } else {
+    throw 'There were no open PRs received from Github';
+  }
+})()
+.then(async() => {
+  log.finish();
+  console.log('Finished finding unknown repo PRs with merge conflicts');
+})
+.catch(err => {
+  log.finish();
+  console.log(err);
+});
