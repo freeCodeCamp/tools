1ac9856f34e3a3394b9e66dcf14f2b19b570c7cc
tbushman <tbushman@pu.bli.sh>
2019-01-04 14:44:04 -0700

    moving server scripts to probot/index.js

--- probot/index.js
+++ probot/index.js
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
 const debug = require('debug')('probot:presolver');
-const Presolver = require('./lib/presolver');
+const Presolver = require('../lib/presolver');
+const config = require('../config');
+// config should be imported before importing any other file
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 async function probotPlugin(robot) {
   const events = [
@@ -13,6 +18,42 @@ async function probotPlugin(robot) {
   ];
 
   robot.on(events, presolve.bind(null, robot));
+  const app = robot.route('/contribute');
+  const { catchAll, pareto, pr, search, info } = require('./server/routes');
+
+   // view engine setup
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(require('express').static(path.join(__dirname, './client/build')));
+  app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    response.header('Access-Control-Allow-Methods', 'GET');
+    next();
+  });
+
+  // app.get('/health-check', (req, res) => res.send('OK'));
+  app.use('/pr', pr);
+  app.use('/search', search);
+  app.use('/pareto', pareto);
+  app.use('/info', info);
+
+  const htmlpath = path.join(__dirname, './client/build/index.html');
+  app.get('/', (request, response) => response.sendFile(htmlpath));
+  app.use('*', catchAll);
+  // connect to mongo db
+  const mongoUri = config.mongo.host;
+  const promise = mongoose.connect(mongoUri, { useNewUrlParser: true });
+  promise.then(() => {
+    console.log('MongoDB is connected');
+  }).catch((err) => {
+    console.log(err);
+    console.log('MongoDB connection unsuccessful');
+  });
+
 }
 
 async function presolve(app, context) {
